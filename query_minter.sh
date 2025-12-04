bd07cd70f5e2b7db7aea65a7a0ec414bcec60a45
MSTBL Team <mstbl@blockchain.com>
2025-12-04 14:08:19 +0200

    Clean up: Remove temporary files and improve documentation

--- query_minter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Query current minter
-
-docker exec mstbl-blockchain wasmd query wasm contract-state smart \
-  wasm14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s0phg4d \
-  '{"minter":{}}' \
-  --node http://localhost:26657
